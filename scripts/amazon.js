b9553b483c02c7058d481fb4af994d9235f8ccc9
nehanlkt <nehanlkt@gmail.com>
2026-04-24 10:47:58 +0530

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -5,36 +5,7 @@ console.log("hello");
 // array of obj
 // calc in cents 1doolar=100cents
 // 1.save data
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-
-}
-
-];
+// data script
 // make sure all have same prop
 
 // 2.generate html
@@ -53,7 +24,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating${product.rating.stars*10}.png">
+              src="images/ratings/rating-${product.rating.stars*10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
@@ -92,13 +63,14 @@ products.forEach((product) => {
     `;
 
 });
-
+console.log("js run");
 
 // 3.put it on page-dom
 // class prdts grid contains all prd details
 
-document.querySelector('.js-product-grid').innerHTML=productshtml;
+document.querySelector('.js-products-grid').innerHTML=productshtml;
 // now generated the html using js no need of that in hrml file deletete thus portion
+// benifit of adding html just by adding objects to array
 
 
 
